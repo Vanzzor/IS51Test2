5fe6fbdd801b87e3f000367476e283f82ac09773
mohammed talib <m.t.talib@hotmail.com>
2022-05-09 20:54:06 -0700

    finished pseudo code

--- is51test2.py
+++ is51test2.py
@@ -5,5 +5,21 @@ reads the total grades and divides by number of grades to obtain average
 prints out average,
 if the value is > average, sends to list , divide count of list on original grades count
 
+"""
+"""
+file=open final.txt
+grades=[]
+grades.append(int(line.strip))
+print="number of grades" len(grades)
+avg=0
+for grade in grades add avg to grades
+average /= len(grade)
+print average, avg
+
+def calculate_percent_above_average()
+count=0
+if grade > avg add to count
+return count*100/len(grades)
 
 """
+
