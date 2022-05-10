9b9e7363aa0b5682727d6a7c8ccaa4d222e07d48
mohammed talib <m.t.talib@hotmail.com>
2022-05-09 20:33:31 -0700

    Finished

---
+++ is51test2.py
@@ -0,0 +1,9 @@
+"""
+Program opens a file that has the grades, named final.txt
+reads the number of lines and prints out grades count
+reads the total grades and divides by number of grades to obtain average
+prints out average,
+if the value is > average, sends to list , divide count of list on original grades count
+
+
+"""
