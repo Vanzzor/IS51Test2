ff10d76e670c44ca756628a1565ee0f240306e50
mohammed talib <m.t.talib@hotmail.com>
2022-05-09 20:43:07 -0700

    added an edit

--- is51test2.py
+++ is51test2.py
@@ -4,6 +4,16 @@ reads the number of lines and prints out grades count
 reads the total grades and divides by number of grades to obtain average
 prints out average,
 if the value is > average, sends to list , divide count of list on original grades count
+close program
+
+"""
+
+"""
+file=open final.txt
+grades=[]
+grades.append(int(line.strip))
+print="number of grades" len(grades)
+
 
 
 """
